ec334c98f4b3d5e6417150b05d5a484a8e745244
Jonathas <jonathaspereira673@gmail.com>
2023-02-07 15:21:13 -0300

    Atualização

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://servidor-summonbr-e1elwlnnv-jhow2004.vercel.app/', { 
+        const response = await fetch('https://servidor-summonbr.vercel.app/', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
