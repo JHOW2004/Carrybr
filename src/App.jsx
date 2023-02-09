6a9fb70fca8dd6dbbbd95a4e2a0a62f2ea992ac6
Jonathas <jonathaspereira673@gmail.com>
2023-02-09 17:31:01 -0300

    At

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,36 @@ import ChatBody from "./components/ChatBody";
 import ChatInput from "./components/ChatInput";
 import { useMutation } from "react-query";
 import { fetchResponse } from "./api";
+import {Animated} from "react-animated-css";
+
+
 function App() {
   const [chat, setChat] = useState([]);
+  const [alt, setAlt] = useState(null)
+  const [larg, setLarg] = useState(null)
+  const [ani1, setAni1] = useState("slideInUp")
+  const [ani2, setAni2] = useState("slideInDown")
+
+  setInterval(() => {
+    var altura = window.screen.height;
+    var largura = window.screen.width;
+    setAlt(altura)
+    setLarg(largura)
+  }, 100)
+
+  setInterval(() => {
+    if(ani1 === "slideInDown"){
+      setAni1("slideInUp")
+    }else{
+      setAni1("slideInDown")
+    }
+
+    if(ani2 === "slideInUp"){
+      setAni2("slideInDown")
+    }else{
+      setAni2("slideInUp")
+    }
+  }, 10000)
 
   const mutation = useMutation({
     mutationFn: () => {
@@ -25,10 +53,24 @@ function App() {
   return (
     <div className="bg-[#000] h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between  align-middle">
       {/* gradients */}
-      <div className="gradient-01 z-0 absolute"></div>
-      <div className="gradient-02 z-0 absolute"></div>
-      <div className="gradient-03 z-0 absolute"></div>
-      <div className="gradient-04 z-0 absolute"></div>
+
+      {larg > 821 &&
+        <>
+          <Animated animationIn="bounceIn"  isVisible={true} animationInDuration ={1000}  className="gradient-01 z-0 absolute"></Animated>
+          <Animated animationIn="bounceIn"  isVisible={true} animationInDuration ={1000} className="gradient-02 z-0 absolute"></Animated>
+          <Animated animationIn="bounceIn" isVisible={true} animationInDuration ={1000} className="gradient-03 z-0 absolute"></Animated>
+          <Animated animationIn="bounceIn"  isVisible={true} animationInDuration ={1000} className="gradient-04 z-0 absolute"></Animated>
+        </>
+      }
+
+      {larg < 821 &&
+        <>
+          <Animated animationIn={ani1}  isVisible={true} animationInDuration ={10000}  className="gradient-01 z-0 absolute"></Animated>
+          <Animated animationIn={ani2}  isVisible={true} animationInDuration ={10000} className="gradient-02 z-0 absolute"></Animated>
+          <Animated animationIn={ani1} isVisible={true} animationInDuration ={10000} className="gradient-03 z-0 absolute"></Animated>
+          <Animated animationIn={ani2}  isVisible={true} animationInDuration ={10000} className="gradient-04 z-0 absolute"></Animated>
+        </>
+      }
 
       {/* header */}
      
